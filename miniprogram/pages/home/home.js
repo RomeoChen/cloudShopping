016e1cf00d6169338e8c71bb07d1b1a2dc6d153b
RomeoChen <18186483358@163.com>
2018-09-19 10:26:23 +0800

    6
    添加home.js中的商品加载提示，封装getProductList()函数

--- miniprogram/pages/home/home.js
+++ miniprogram/pages/home/home.js
@@ -1,66 +1,98 @@
 // pages/home/home.js
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        //   商品列表
+        productList: [
+            // {
+            //     id: 1,
+            //     image: 'https://s3.cn-north-1.amazonaws.com.cn/u-img/product1.jpg',
+            //     name: '商品1',
+            //     price: 100,
+            //     source: '国内·广东',
+            // },
+        ]
+    },
 
-  },
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function(options) {
+        this.getProductList()
+    },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+    // 获得商品列表
+    getProductList() {
+        wx.showLoading({
+            title: '商品加载中...',
+        })
+        wx.cloud.callFunction({
+            name: 'product',
+            success: res => {
+                wx.hideLoading()
+                console.log(res)
+                this.setData({
+                    productList: res.result.data
+                })
+            },
+            fail: () => {
+                wx.hideLoading()
+                wx.showToast({
+                    title: '商品加载失败！',
+                })
+            }
+        })
+    },
 
-  },
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function() {
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+    },
 
-  },
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function() {
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
+    },
 
-  },
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function() {
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
+    },
 
-  },
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function() {
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
+    },
 
-  },
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function() {
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
+    },
 
-  },
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function() {
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
+    },
 
-  },
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function() {
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
+    }
 })
